15a837bdad9405a0321b48a33304ee345c6280b0
mc-b <marcel.bernet@ch-open.ch>
2018-04-25 08:00:18 +0200

    Kursumgebung - Initial Commit

---
+++ dashboard.bat
@@ -0,0 +1,12 @@
+REM Startet den Default Browser mit der Dashboard Startseite und den Port Forward 
+
+cd /d %~d0%~p0
+
+set DOCKER_HOST=tcp://192.168.60.100:2376
+set DOCKER_TLS_VERIFY=1
+set DOCKER_CERT_PATH=%~d0%~p0.docker
+set KUBECONFIG=%~d0%~p0.kube\config
+set PATH=%~d0%~p0bin;%PATH%
+ 
+start http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/. /B
+kubectl proxy
