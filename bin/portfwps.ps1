b67eb00f30ea071d08d2ade2825e8c744c70b48e
mc-b <marcel.bernet@ch-open.ch>
2018-05-22 19:17:53 +0200

    Korrektur Port

--- bin/portfwps.ps1
+++ bin/portfwps.ps1
@@ -1,3 +1,4 @@
+Set-PSDebug -Trace 1
 
 netsh interface portproxy add v4tov4 listenaddress=localhost listenport=32100  connectaddress=$args connectport=32100
 netsh interface portproxy add v4tov4 listenaddress=localhost listenport=32200  connectaddress=$args connectport=32200
@@ -5,12 +6,12 @@ netsh interface portproxy add v4tov4 listenaddress=localhost listenport=32300  c
 
 netsh interface portproxy add v4tov4 listenaddress=localhost listenport=32080  connectaddress=$args connectport=32080
 netsh interface portproxy add v4tov4 listenaddress=localhost listenport=32090  connectaddress=$args connectport=32090
-netsh interface portproxy add v4tov4 listenaddress=localhost listenport=31280  connectaddress=$args connectport=32180
+netsh interface portproxy add v4tov4 listenaddress=localhost listenport=32180  connectaddress=$args connectport=32180
 netsh interface portproxy add v4tov4 listenaddress=localhost listenport=32280  connectaddress=$args connectport=32280
 
 netsh interface portproxy add v4tov4 listenaddress=localhost listenport=30443  connectaddress=$args connectport=30443
 netsh interface portproxy add v4tov4 listenaddress=localhost listenport=6443  connectaddress=$args connectport=6443
 
 netsh interface portproxy add v4tov4 listenaddress=localhost listenport=32088  connectaddress=$args connectport=32088
-netsh interface portproxy add v4tov4 listenaddress=localhost listenport=32288  connectaddress=$args connectport=32288
+netsh interface portproxy add v4tov4 listenaddress=localhost listenport=32388  connectaddress=$args connectport=32388
 
