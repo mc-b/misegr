2a0b510898f4ac03c3bfda5f10930b2834b86b34
mc-b <marcel.bernet@ch-open.ch>
2018-08-05 16:41:35 +0200

    Installationsscript

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#	Installationsscript misegr
+
+# Kubernetes Namespaces Microservices
+for	n in scsesi, ms-kafka, ms-kubernetes
+do
+	kubectl create namespace ${n}
+done
+
+# Jupyter Scripte etc. Allgemein verfuegbar machen
+cp -rpv data/* /data/
